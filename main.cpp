5eded8485cc2cb7e1b2730e0ff0ac33ba716d31d
Marci <Marci@bannana.chu.cam.ac.uk>
2015-11-06 16:06:01 +0000

    main.cpp created

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Marci
+ *
+ * Created on November 6, 2015, 4:05 PM
+ */
+
+#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("cock");
+    return 0;
+}
+
